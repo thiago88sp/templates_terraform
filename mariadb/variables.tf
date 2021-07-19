e688f28bb0b558e1a7209a9ca9bb72e01f7172dd
Thiago Souza Pontes <thiago88sp@gmail.com>
2021-07-19 12:16:31 -0300

    Alteração do variables do mariadb

--- mariadb/variables.tf
+++ mariadb/variables.tf
@@ -1,7 +1,10 @@
+#variable "prefix" {
+#  type        =  string    
+#  description = "The prefix which should be used for all resources in this example"
+#}
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
-
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
