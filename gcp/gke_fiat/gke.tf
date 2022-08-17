4723b5bfd4e18c71ce38878ced4fae1c85673256
Thiago Souza Pontes <thiago88sp@gmail.com>
2022-08-17 11:45:58 -0300

    GKE

--- gcp/gke_fiat/gke.tf
+++ gcp/gke_fiat/gke.tf
@@ -10,7 +10,7 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
+  subnetwork = google_compute_subnetwork.gke_subnet.name
 }
 
 # Separately Managed Node Pool
--- gcp/gke_fiat/variables.tf
+++ gcp/gke_fiat/variables.tf
@@ -12,3 +12,16 @@ variable "gke_num_nodes" {
   default     = 2
   description = "number of gke nodes"
 }
+
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
