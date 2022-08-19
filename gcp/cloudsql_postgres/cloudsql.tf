0301d2c2595db1e2fc51acaf488ff3315a80b718
Thiago Souza Pontes <thiago88sp@gmail.com>
2022-08-19 10:37:41 -0300

    Criação de CloudSQL (Postgres)

--- gcp/cloudsql_postgres/cloudsql.tf
+++ gcp/cloudsql_postgres/cloudsql.tf
@@ -4,10 +4,10 @@ resource "random_id" "db_name_suffix" {
 
 resource "google_sql_database_instance" "instance" {
 
-  name             = "private-instance-${random_id.db_name_suffix.hex}"
-  region           = var.region
-  database_version = var.postgres_version
-  project          = var.gcp_project_id
+  name                    = "private-instance-${random_id.db_name_suffix.hex}"
+  region                  = var.region
+  database_version        = var.postgres_version
+  project                 = var.gcp_project_id
 
   settings {
     tier = "db-f1-micro"
@@ -16,5 +16,18 @@ resource "google_sql_database_instance" "instance" {
     #  private_network = data.google_compute_network.vpc.private_network.id
     #}
     availability_type = var.availability_type
+    disk_autoresize         = var.disk_autoresize
+    disk_autoresize_limit   = var.disk_autoresize_limit
+    disk_size               = var.disk_size
+    disk_type               = var.disk_type
+    pricing_plan            = var.pricing_plan
+
+    maintenance_window {
+      day  = var.maintenance_window_day
+      hour = var.maintenance_window_hour  
+    }
+
   }
+
+
 }
--- gcp/cloudsql_postgres/variables.tf
+++ gcp/cloudsql_postgres/variables.tf
@@ -15,6 +15,48 @@ variable "region" {
 }
 
 variable "availability_type" {
-  description = "The availability type of the Cloud SQL instance"
+  description = "The availability type for the master instance.This is only used to set up high availability for the PostgreSQL instance. Can be either `ZONAL` or `REGIONAL`."
   type        = string
+  default     = "ZONAL"
+}
+
+variable "disk_autoresize" {
+  description = "Configuration to increase storage size."
+  type        = bool
+  default     = true
+}
+
+variable "disk_autoresize_limit" {
+  description = "The maximum size to which storage can be auto increased."
+  type        = number
+  default     = 0
+}
+
+variable "disk_size" {
+  description = "The disk size for the master instance."
+  default     = 10
+}
+
+variable "disk_type" {
+  description = "The disk type for the master instance."
+  type        = string
+  default     = "PD_SSD"
+}
+
+variable "pricing_plan" {
+  description = "The pricing plan for the master instance."
+  type        = string
+  default     = "PER_USE"
+}
+
+variable "maintenance_window_day" {
+  description = "The day of week (1-7) for the master instance maintenance."
+  type        = number
+  default     = 1
+}
+
+variable "maintenance_window_hour" {
+  description = "The hour of day (0-23) maintenance window for the master instance maintenance."
+  type        = number
+  default     = 23
 }
