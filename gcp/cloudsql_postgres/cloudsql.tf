c3bb6d0b3dfc58ea16ad39186d6055ec1273d339
Thiago Souza Pontes <thiago88sp@gmail.com>
2022-08-26 16:14:30 -0300

    Criação de Módulo Postgres

--- gcp/cloudsql_postgres/cloudsql.tf
+++ gcp/cloudsql_postgres/cloudsql.tf
@@ -1,102 +1,13 @@
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
-# NETWORK PARAMETERS
+# CloudSQL - Postgres
 # ---------------------------------------------------------------------------------------------------------------------
 
-#name = data.google_compute_network.name
+module "cloudsqlpostgres" {
+  source = "./modules/postgres" 
 
-resource "google_compute_global_address" "private_ip_address" {
+  gcp_project_id = "terraformproject-359719"
+  region     = "southamerica-east1"
+  availability_type = "ZONAL"
+  zone = "southamerica-east1-a"
 
-  name          = "${var.gcp_project_id}-private-ip-adress"
-  project       = var.gcp_project_id
-  purpose       = var.purpose
-  address_type  = var.address_type
-  prefix_length = var.prefix_length
-  network       = data.google_compute_network.vpc.id
-
-}
-
-resource "google_service_networking_connection" "private_vpc_connection" {
-
- network                 = data.google_compute_network.vpc.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-}
-
-
-# ---------------------------------------------------------------------------------------------------------------------
-# CLOUSQL (POSGRES) PARAMETERS
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "google_sql_database_instance" "instance" {
-
-  #name                    = "private-instance-${random_id.db_name_suffix.hex}"
-  name                    = "${var.gcp_project_id}-postgres"
-  region                  = var.region
-  database_version        = var.postgres_version
-  project                 = var.gcp_project_id
-
-
-  # To make it easier to test this example, we are disabling deletion protection so we can destroy the databases
-  # during the tests. By default, we recommend setting deletion_protection to true, to ensure database instances are
-  # not inadvertently destroyed.
-  deletion_protection     = false
-
-  depends_on = [google_service_networking_connection.private_vpc_connection]
-
-
-  settings {
-    tier = "db-f1-micro"
-    ip_configuration {
-      ipv4_enabled    = false
-      private_network = data.google_compute_network.vpc.id
-      #allocated_ip_range = data.google_compute_subnetwork.postgres_subnet.secondary_ip_range.range_name
-    }
-    availability_type = var.availability_type
-    disk_autoresize         = var.disk_autoresize
-    disk_autoresize_limit   = var.disk_autoresize_limit
-    disk_size               = var.disk_size
-    disk_type               = var.disk_type
-    pricing_plan            = var.pricing_plan
-    activation_policy       = var.activation_policy
-
-    maintenance_window {
-      day  = var.maintenance_window_day
-      hour = var.maintenance_window_hour  
-    }
-
-    location_preference {
-      zone  = var.zone
-      secondary_zone  = var.secondary_zone
-
-    }
-
-    user_labels = {
-      env = "dev"
-      purpose = "fidis"
-    }
-
-    dynamic "backup_configuration" {
-      for_each = [var.backup_configuration]
-      content {
-        binary_log_enabled             = false
-        enabled                        = local.backups_enabled
-        start_time                     = lookup(backup_configuration.value, "start_time", null)
-        location                       = lookup(backup_configuration.value, "location", null)
-        point_in_time_recovery_enabled = local.point_in_time_recovery_enabled
-        transaction_log_retention_days = lookup(backup_configuration.value, "transaction_log_retention_days", null)
-
-        dynamic "backup_retention_settings" {
-          for_each = local.retained_backups != null || local.retention_unit != null ? [var.backup_configuration] : []
-          content {
-            retained_backups = local.retained_backups
-            retention_unit   = local.retention_unit
-          }
-        }
-      }
-    }
-  }
 }
--- gcp/cloudsql_postgres/variables.tf
+++ gcp/cloudsql_postgres/variables.tf
@@ -3,123 +3,17 @@ variable "gcp_project_id" {
   type = string
 }
 
-variable "postgres_version" {
-  description = "The engine version of the database, e.g. `POSTGRES_14`. See https://cloud.google.com/sql/docs/db-versions for supported versions."
-  type        = string
-  default     = "POSTGRES_14"
-}
-
 variable "region" {
-  description = "The region to host the database in."
-  type        = string
+  description = "The project ID to host the database in."
+  type = string
 }
 
 variable "availability_type" {
-  description = "The availability type for the master instance.This is only used to set up high availability for the PostgreSQL instance. Can be either `ZONAL` or `REGIONAL`."
-  type        = string
-  default     = "ZONAL"
-}
-
-variable "disk_autoresize" {
-  description = "Configuration to increase storage size."
-  type        = bool
-  default     = true
-}
-
-variable "disk_autoresize_limit" {
-  description = "The maximum size to which storage can be auto increased."
-  type        = number
-  default     = 0
-}
-
-variable "disk_size" {
-  description = "The disk size for the master instance."
-  default     = 10
-}
-
-variable "disk_type" {
-  description = "The disk type for the master instance."
-  type        = string
-  default     = "PD_SSD"
-}
-
-variable "pricing_plan" {
-  description = "The pricing plan for the master instance."
-  type        = string
-  default     = "PER_USE"
-}
-
-variable "maintenance_window_day" {
-  description = "The day of week (1-7) for the master instance maintenance."
-  type        = number
-  default     = 1
-}
-
-variable "maintenance_window_hour" {
-  description = "The hour of day (0-23) maintenance window for the master instance maintenance."
-  type        = number
-  default     = 23
+  description = "The project ID to host the database in."
+  type = string
 }
 
 variable "zone" {
-  type        = string
-  description = "The zone for the master instance, it should be something like: `us-central1-a`, `us-east1-c`."
-}
-
-variable "secondary_zone" {
-  type        = string
-  description = "The zone for the master instance, it should be something like: `us-central1-a`, `us-east1-c`."
-  default = ""
-}
-
-variable "activation_policy" {
-  description = "The activation policy for the master instance.Can be either `ALWAYS`, `NEVER` or `ON_DEMAND`."
-  type        = string
-  default     = "ALWAYS"
-}
-
-variable "backup_configuration" {
-  description = "The backup_configuration settings subblock for the database setings"
-  type = object({
-    enabled                        = bool
-    start_time                     = string
-    location                       = string
-    point_in_time_recovery_enabled = bool
-    transaction_log_retention_days = string
-    retained_backups               = number
-    retention_unit                 = string
-  })
-  default = {
-    enabled                        = false
-    start_time                     = null
-    location                       = null
-    point_in_time_recovery_enabled = false
-    transaction_log_retention_days = null
-    retained_backups               = null
-    retention_unit                 = null
-  }
-}
-
-variable "user_labels" {
-  description = "The key/value labels for the master instances."
-  type        = map(string)
-  default     = {}
-}
-
-variable "purpose" {
-  type      = string
-  description = "The purpose of the resource. Possible values include: VPC_PEERING - for peer networks and PRIVATE_SERVICE_CONNECT - for (Beta only) Private Service Connect networks"
-  default     = "VPC_PEERING"
-}
-
-variable "address_type" {
-  type    = string
-  description = "The type of the address to reserve. EXTERNAL indicates public/external single IP address. INTERNAL indicates internal IP ranges belonging to some network. Default value is EXTERNAL. Possible values are EXTERNAL and INTERNAL."
-  default = "INTERNAL"
-}
-
-variable "prefix_length" {
-  type  = number
-  description = "The prefix length of the IP range. If not present, it means the address field is a single IP address. This field is not applicable to addresses with addressType=EXTERNAL, or addressType=INTERNAL when purpose=PRIVATE_SERVICE_CONNECT"
-  default = 16
+  description = "The project ID to host the database in."
+  type = string
 }
